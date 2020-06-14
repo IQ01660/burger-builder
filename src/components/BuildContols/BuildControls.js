09ed35014f1490e1d00d40af5f53a716dde6e977
Ikram Gabiyev <ikram20011127@gmail.com>
2020-06-14 11:33:30 +0400

    done withe BurgerControls and displaying price

--- src/components/BuildContols/BuildControls.js
+++ src/components/BuildContols/BuildControls.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: "Salad", type: 'salad'}, 
+    {label: "Bacon", type: 'bacon'}, 
+    {label: "Cheese", type: 'cheese'}, 
+    {label: "Meat", type: 'meat'}, 
+];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        
+        <p>Current price: <strong>${props.price.toFixed(2)}</strong></p>
+        {controls.map((control, idx, controls) => {
+            return <BuildControl key={control.label} added={() => props.ingredientAdded(control.type)} removed={() => props.ingredientRemoved(control.type)} label={control.label} disabled={props.ingredients[control.type] === 0}/>
+        })}
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,14 @@ import Aux from './../../hoc/Aux';
 
 //component imports
 import Burger from './../../components/Burger/Burger';
+import BuildControls from './../../components/BuildContols/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.3,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
 
 /**
  * Contains the Burger Builder page
@@ -19,6 +27,31 @@ class BurgerBuilder extends React.Component
             cheese: 0,
             meat: 0,
         },
+        totalPrice: 4
+    };
+
+    addIngredientHandler = (type) => {
+        this.setState((prevState, props) => {
+            const updatedState = {...prevState};
+            updatedState.ingredients[type]++;
+            updatedState.totalPrice += INGREDIENT_PRICES[type];
+
+            return updatedState;
+        });
+    };
+
+    removeIngredientHandler = (type) => {
+        this.setState((prevState, props) => {
+            const updatedState = {...prevState};
+
+            if(updatedState.ingredients[type])
+            {
+                updatedState.ingredients[type]--;
+                updatedState.totalPrice -= INGREDIENT_PRICES[type];
+            }
+            
+            return updatedState;
+        });
     };
 
     render() 
@@ -26,7 +59,7 @@ class BurgerBuilder extends React.Component
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls price={this.state.totalPrice} ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler} ingredients={this.state.ingredients}/>
             </Aux>
         );
     }
